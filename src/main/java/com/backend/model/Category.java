593b24abdce087cfba1f6347f425dc53cfdac2c5
Osama Elshafei <alshafyosama147@gmail.com>
2023-03-19 21:10:18 +0200

    Create-relations

--- src/main/java/com/backend/model/Category.java
+++ src/main/java/com/backend/model/Category.java
@@ -4,13 +4,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @Entity
 public class Category extends categoryorder{
 
-
+    @OneToMany
+    private Set<Order>orders;
 
 
 }
--- src/main/java/com/backend/model/Order.java
+++ src/main/java/com/backend/model/Order.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +15,10 @@ public class Order extends categoryorder{
 
     private int price;
     private String img;
+    @Lob
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "id_category")
+private Category category;
 
 }
